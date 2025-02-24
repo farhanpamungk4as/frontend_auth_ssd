c432c33f555da9b59e79d99f5438ca58fe7882c7
farhan pamungkas <fpamungkas100@gmail.com>
2025-02-25 01:09:54 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./components/register";
 import Login from "./components/login";
 import Home from "./pages/home";
@@ -7,6 +7,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/register" replace />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/home" element={<Home />} />
